44a0d282a2cf19dfd09d3d57099620ddd112d59a
Nehameena-R <73383828+Nehameena-R@users.noreply.github.com>
2020-12-18 23:53:08 +0530

    Add files via upload

---
+++ check_Fermat's last thm.py
@@ -0,0 +1,16 @@
+def check_fermat(a, b, c, n):
+    if n > 2 and (a**n + b**n == c**n):
+        print("Holy smokes, Fermat was wrong!")
+    else:
+        print("No, that doesn’t work.")
+
+def check_numbers():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    c = int(input("Enter c: "))
+    n = int(input("Enter n: "))
+    return check_fermat(a, b, c, n)
+
+check_numbers()
+
+
